a06e3983cbc3b353c186947140f0272cda32f92e
Barrenkala <x3gy@M00973421.nordstrom.net>
2017-05-01 22:46:52 -0700

    Added some more methods

--- configo.go
+++ configo.go
@@ -32,11 +32,11 @@ func New(environment string) (*Config, error) {
 	cfg := Config{environment:environment, configData:make(map[string]string) }
 
 	err := cfg.readFromFile(configFolder + "/" + configFile, false)
-	if(err != nil){
+	if err != nil {
 		return nil, err
 	}
 	err = cfg.readFromFile(configFolder + "/" + environment + "." + configFile, true)
-	if(err != nil){
+	if err != nil {
 		return nil, err
 	}
 
@@ -55,59 +55,122 @@ func (c *Config) GetKeys() []string {
 	return c.configKeys
 }
 
-func (c *Config) GetString(key string, defaultVal string) string {
-	val, _ := c.configData[key]
+func (c *Config) GetString(key string) (string, error) {
+	val, ok := c.configData[key]
 
-	if len(val) == 0 {
+	if !ok {
+		return "", errors.New("Config entry with key '" + key + "' does not exist")
+	}
+
+	return val, nil
+}
+
+func (c *Config) GetStringOrDefault(key string, defaultVal string) string {
+	val, err := c.GetString(key)
+
+	if err != nil {
 		val = defaultVal
 	}
 
 	return val
 }
 
-func (c *Config) GetInt(key string, defaultVal int64) int64 {
-	val, _ := c.configData[key]
+func (c *Config) GetInt(key string) (int64, error) {
+	val, ok := c.configData[key]
+
+	if !ok {
+		return 0, errors.New("Config entry with key '" + key + "' does not exist")
+	}
 
 	intVal, err := strconv.ParseInt(val, 10, 64)
 	if err != nil {
-		return defaultVal
+		return 0, errors.New("Config value '" + val + "' for key '" + key + "' is not integer")
 	}
-	return intVal
+	return intVal, nil
 }
 
-func (c *Config) GetFloat(key string, defaultVal float64) float64 {
-	val, _ := c.configData[key]
+func (c *Config) GetIntOrDefault(key string, defaultVal int64) int64 {
+	val, err := c.GetInt(key)
+
+	if err != nil {
+		return defaultVal
+	}
+	return val
+}
+
+func (c *Config) GetFloat(key string) (float64, error) {
+	val, ok := c.configData[key]
+
+	if !ok {
+		return 0, errors.New("Config entry with key '" + key + "' does not exist")
+	}
 
 	floatVal, err := strconv.ParseFloat(val, 64)
 	if err != nil {
-		return defaultVal
+		return 0, errors.New("Config value '" + val + "' for key '" + key + "' is not float")
 	}
-	return floatVal
+	return floatVal, nil
 }
 
-func (c *Config) GetUInt(key string, defaultVal uint64) uint64 {
-	val, _ := c.configData[key]
+func (c *Config) GetFloatOrDefault(key string, defaultVal float64) float64 {
+	val, err := c.GetFloat(key)
+
+	if err != nil {
+		return defaultVal
+	}
+	return val
+}
+
+func (c *Config) GetUInt(key string) (uint64, error) {
+	val, ok := c.configData[key]
+
+	if !ok {
+		return 0, errors.New("Config entry with key '" + key + "' does not exist")
+	}
 
 	uiVal, err := strconv.ParseUint(val, 10,64)
 	if err != nil {
-		return defaultVal
+		return 0, errors.New("Config value '" + val + "' for key '" + key + "' is not unsigned integer")
 	}
-	return uiVal
+	return uiVal, nil
 }
 
-func (c *Config) GetBool(key string, defaultVal bool) bool {
-	val, _ := c.configData[key]
+func (c *Config) GetUIntOrDefault(key string, defaultVal uint64) uint64 {
+	val, err := c.GetUInt(key)
 
-	boolVal, err := strconv.ParseBool(val)
 	if err != nil {
 		return defaultVal
 	}
-	return boolVal
+	return val
+}
+
+func (c *Config) GetBool(key string) (bool, error) {
+	val, ok := c.configData[key]
+
+	if !ok {
+		return false, errors.New("Config entry with key '" + key + "' does not exist")
+	}
+
+	boolVal, err := strconv.ParseBool(val)
+	if err != nil {
+		return false, errors.New("Config value '" + val + "' for key '" + key + "' is not boolean")
+	}
+	return boolVal, nil
+}
+
+func (c *Config) GetBoolOrDefault(key string, defaultVal bool) bool {
+	val, err := c.GetBool(key)
+
+	if err != nil {
+		return defaultVal
+	}
+	return val
 }
 
 func (c *Config) readFromFile(configFile string, ignoreFileError bool) error {
 	file, err := os.Open(configFile)
 	defer file.Close()
+
 	if !ignoreFileError {
 		if err != nil {
 			return err
